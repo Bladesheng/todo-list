93b0dfb85df70b63a8d52af5608adbd1286d4d04
Bladesheng <keadr23@gmail.com>
2022-07-30 10:00:23 +0200

    Remove redundant code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import "./style.scss";
-import Storage from "./modules/Storage";
-import UI from "./modules/UI";
-import DOM from "./modules/DOM";
 
+// initialize storage before UI
+import Storage from "./modules/Storage";
+
+import UI from "./modules/UI";
 
 UI.init();
-
-
-// debug
-
-//Storage.createList("test list 2", 0);
-//Storage.createCard("test card", "some description", 3, 0, 2);
-
-//Storage.changeCardTitle("feed cat", 0, 0, 1);
