04651a1a069fb7593752318135783cde23c5a680
Bladesheng <keadr23@gmail.com>
2022-08-03 08:15:08 +0200

    Style the kebab menu

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -260,9 +260,8 @@ export default class DOM {
     const kebabMenu = document.createElement("div");
     kebabMenu.classList.add("menu");
 
-    // trash icon "M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
     const removeBtn = document.createElement("button");
-    removeBtn.textContent = "Delete this list";
+    removeBtn.textContent = "Remove this list";
     removeBtn.classList.add("removeList");
     kebabMenu.appendChild(removeBtn);
     kebabBtn.parentNode.appendChild(kebabMenu);
